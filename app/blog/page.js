b319f9298f94ed41d8f8f1e51cae042f93670c3f
Saidur <saidursd@gmail.com>
2026-01-03 09:09:57 -0600

    blog issue fix

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,7 +4,12 @@ import { personalData } from "@/utils/data/personal-data";
 import BlogCard from "../components/homepage/blog/blog-card";
 
 async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(
+    `https://dev.to/api/articles?username=${personalData.devUsername}`,
+    {
+      next: { revalidate: 3600 } // 1 hour
+    }
+  );
   
   if (!res.ok) {
     throw new Error('Failed to fetch data')
