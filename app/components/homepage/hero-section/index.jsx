d5ac3b9001f2cd9fa049031a3f952a97f2d70a06
Saidur Rahman <info.sraurora@gmail.com>
2026-06-11 16:04:32 +0600

    headline udpated, removed unnesseary word

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -24,11 +24,10 @@ function HeroSection() {
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             <br />
-            Hi, I’m  {' '}
-            <span className=" text-pink-500">{personalData.name}</span>
-            {` - a professional `}
-            <span className=" text-[#16f2b3]">{personalData.designation}</span>
-            {` with an AI-first mindset `}
+            Hi, I'm {' '}
+            <span className="text-pink-500">{personalData.name}</span>
+            {` - `}
+            <span className="text-[#16f2b3]">{personalData.designation}</span>
           </h1>
 
           <div className="my-12 flex items-center gap-5">
