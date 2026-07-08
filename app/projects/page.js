bb631419f7cbce52c5e986caebc47428fdb1b91e
Saidur Rahman <info.sraurora@gmail.com>
2026-07-08 09:20:38 +0600

    bug fix added

--- app/projects/page.js
+++ app/projects/page.js
@@ -9,11 +9,11 @@ function page() {
     <div className="py-8">
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
+          <span className="w-8 sm:w-24 h-[2px] bg-[#1a1443]"></span>
+          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl sm:text-2xl rounded-md whitespace-nowrap">
             All Projects
           </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          <span className="w-8 sm:w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
 
